97229772362d700ad5906735809a424a15bd51f6
Felipe Norato <fnoratol@gmail.com>
2022-05-11 17:15:24 -0300

    create header component

--- libs/shared/ui-components/src/lib/shared-ui-components.module.ts
+++ libs/shared/ui-components/src/lib/shared-ui-components.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class SharedUiComponentsModule {}
